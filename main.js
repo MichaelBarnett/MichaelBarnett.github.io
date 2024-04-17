6c5689fca1909705f06fb2b4503f373d63f81988
Michael Michael <MichaelBarnett@users.noreply.github.com>
2024-04-17 18:39:22 +1200

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry( 2, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
